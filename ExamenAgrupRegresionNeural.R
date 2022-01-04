c005e15ee5cb3f0f28e095f900c37fe3280e8c0d
dianaagustinaf <48896415+dianaagustinaf@users.noreply.github.com>
2022-01-04 17:59:58 +0000

    Create ExamenAgrupRegresionNeural.R

---
+++ ExamenAgrupRegresionNeural.R
@@ -0,0 +1,29 @@
+CONSIGNAS:
+
+El archivo combustibles.csv tiene datos sobre distintos vehículos que utilizan nafta, diesel o gas.
+
+Importar los datos del csv y usar set.seed(#TU DNI#)
+
+1-
+
+ a. Realizar un agrupamiento jerárquico de 6 grupos sin usar la columna Combustible.
+
+ b. Realizar un gráfico con eje x=Respuesta. eje y=Ruido. color=agrupamiento según punto a.
+
+ c. Identificar cuales grupos pertenecerían a cada uno de los combustibles. Cual es la proporción de vehículos que quedarían agrupados erróneamente?
+
+ 
+
+2- Utilizando el 75% de los datos para entrenamiento:
+
+ a. Realizar una regresión lineal sin usar la columna combustible para estimar la velocidad máxima.
+
+ b. Cuál sería la velocidad máxima de un vehículo con ruido, respuesta y volumen promedios de todos los vehículos?
+
+
+
+3- Utilizando el 70% de los datos para entrenamiento:
+
+ a. Realizar una red neuronal para estimar el combustible utilizado de los vehículos, usando una capa oculta de 5 neuronas y la función de activación "tanh"
+
+ b. Realizar y analizar la matriz de confusión con los datos de testeo.
