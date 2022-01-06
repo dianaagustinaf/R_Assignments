8ff4a5ef14638c7cb6741c46d434cd880ba2573e
dianaagustinaf <48896415+dianaagustinaf@users.noreply.github.com>
2022-01-06 23:41:13 +0000

    Punto 1 Agrupamiento

---
+++ ExamenAgrupRegresionNeuralResolucion.R
@@ -0,0 +1,71 @@
+#leo csv
+combu<-read.csv("combustibles.csv",stringsAsFactors = TRUE)
+set.seed(35971187)
+
+# 1-
+#   a. Realizar un agrupamiento jerárquico de 6 grupos sin usar la columna Combustible.
+
+
+#hago una primera lectura de los datos para entender el contenido con summary y head
+summary(combu)
+head(combu)
+
+# Saco columna Combustible, escalo las numericas y calculo distincia con dist() 
+combu2 <- combu[,-1]
+summary(combu2)
+
+distCombu <- dist(scale(combu2))
+summary(distCombu)
+head(distCombu)
+
+#Aplico hclust
+hc <- hclust(distCombu)
+
+#Grafico
+plot(hc)
+
+#Agrupo en cuadradantes
+#muestra las agrupaciones en la viz
+rect.hclust(hc,k=6, border=3:10)
+
+#Agrego columna grupo
+combu2 <- cbind(combu, Grupo=cutree(hc,k=6))
+head(combu2)
+
+
+# b. Realizar un gráfico con eje x=Respuesta. eje y=Ruido. color=agrupamiento 
+# según punto a.
+
+
+#Grafico con ggplot  
+#(le agrego también shape con Combustible para una mejor identificación)
+library(ggplot2)
+summary(combu2)
+ggplot(combu2,aes(x=Respuesta,y=Ruido,col=as.factor(Grupo),shape=as.factor(Combustible))) + geom_point()
+
+
+# c. Identificar cuales grupos pertenecerían a cada uno de los combustibles. 
+# Cual es la proporción de vehículos que quedarían agrupados erróneamente?
+
+
+#Matriz de confusion para ver tipo de combustible por grupo:
+table(combu2$Combustible,combu2$Grupo)
+
+#         1  2  3  4  5  6
+# Diesel  0  0 11  7  4  6
+# Gas     0  0  4 10 23  0
+# Nafta  14 21  0  0  0  0
+
+# 1c. Analizando el gráfico y la matriz puedo decir que:
+
+# Los grupos más claros son el 1 y el 2, que pertenecen al combustible Nafta
+# Quedaron bien agrupados y sin error
+# Luego los grupos 3,4,5 y 6 son más confusos porque quedaron más mezclados el Diesel y el Gas
+# El gas quedó principalmente agrupado en el 4 y el 5 (mayor presencia en el 5)
+# Pero Diesel tiene presencia en los 4 grupos (mayor presencia en 3 y 4)
+#
+# Proporcion de agrupamiento erronea:
+# 14 de Gas (grupo 3 y 4) y 10 de Diesel (grupo 5 y 6)= 24/100
+# El 24%
+
+############################################################################################################
